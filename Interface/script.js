80d3ffd142660ff1438463bfa310e972b2d3fd2c
linkad0 <phpg25@gmail.com>
2022-11-07 19:28:57 -0300

    Inicial Commit

---
+++ Interface/script.js
@@ -0,0 +1,32 @@
+
+console.log("Funcionando");
+let toolbox = {
+    "kind": "flyoutToolbox",
+    "contents": [
+      {
+        "kind": "block",
+        "type": "controls_if"
+      },
+      {
+        "kind": "block",
+        "type": "logic_compare"
+      },
+      {
+        "kind": "block",
+        "type": "math_number"
+      },
+      {
+        "kind": "block",
+        "type": "math_arithmetic"
+      },
+      {
+        "kind": "block",
+        "type": "text"
+      },
+      {
+        "kind": "block",
+        "type": "text_print"
+      },
+    ]
+  }
+  let workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox});
