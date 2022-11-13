465ac23abc13a51273565056842151be7a52d097
@bellamariz <bellamariz@aluno.puc-rio.br>
2022-11-13 17:17:46 -0300

    Given a number of encoder gaps, get corresponding arc length and angle

---
+++ motor/encoder.py
@@ -0,0 +1,15 @@
+import math
+
+GAPS_ENCODER = 20
+RAIO_INT_RODA = 4.2 # cm
+RAIO_EXT_RODA = 6.8 # cm
+CIRCUMF_RODA = 2*math.pi*RAIO_EXT_RODA
+
+
+def arc_length(gaps):
+    return (gaps * CIRCUMF_RODA)/GAPS_ENCODER
+
+def angle(gaps): # in degrees
+    return (arc_length(gaps)/RAIO_EXT_RODA)*(180/math.pi)
+
+
