5f325497d8b9f24f5aac585842ff465299307204
@bellamariz <bellamariz@aluno.puc-rio.br>
2022-11-13 16:28:44 -0300

    Add motor control to project

---
+++ motor/control.py
@@ -0,0 +1,57 @@
+from tkgpio import TkCircuit
+from json import load
+
+# initialize the circuit inside the GUI
+with open("motor/simulation.json", "r") as file:
+    config_analog = load(file)
+
+analog = TkCircuit(config_analog)
+
+@analog.run
+def main():
+    
+    from gpiozero import Motor, MCP3008, Button
+    from time import sleep
+
+    MOTOR_SPEED = 0.5
+
+    motorEsq = Motor(22, 23)
+    motorDir = Motor(24, 25)
+
+    frente = Button(11)
+    tras = Button(12)
+    esq = Button(13)
+    dir = Button(14)
+    parar = Button(15)
+
+
+    def forward():
+        motorEsq.forward(MOTOR_SPEED)
+        motorDir.forward(MOTOR_SPEED)
+
+    def backward():
+        motorEsq.backward(MOTOR_SPEED)
+        motorDir.backward(MOTOR_SPEED)
+
+    def left():
+        motorEsq.backward(MOTOR_SPEED)
+        motorDir.forward(MOTOR_SPEED)
+
+    def right():
+        motorEsq.forward(MOTOR_SPEED)
+        motorDir.backward(MOTOR_SPEED)
+
+    def stop():
+        motorEsq.stop()
+        motorDir.stop()
+
+
+    while True:
+        frente.when_pressed = forward
+        tras.when_pressed = backward
+        esq.when_pressed = left
+        dir.when_pressed = right
+        parar.when_pressed = stop
+
+        sleep(0.05)
+
