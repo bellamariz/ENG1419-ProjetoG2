339535b603fdd47618d771710dd8f16dc03d5431
linkad0 <phpg25@gmail.com>
2022-11-16 15:46:16 -0300

    Adição de blocos ja prontos do Google Blockly

--- Interface/script.js
+++ Interface/script.js
@@ -1,32 +1,67 @@
 
 console.log("Funcionando");
-let toolbox = {
-    "kind": "flyoutToolbox",
-    "contents": [
-      {
-        "kind": "block",
-        "type": "controls_if"
-      },
-      {
-        "kind": "block",
-        "type": "logic_compare"
-      },
-      {
-        "kind": "block",
-        "type": "math_number"
-      },
-      {
-        "kind": "block",
-        "type": "math_arithmetic"
-      },
-      {
-        "kind": "block",
-        "type": "text"
-      },
-      {
-        "kind": "block",
-        "type": "text_print"
-      },
-    ]
-  }
-  let workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox});
+
+Blockly.Blocks.teste_blockly = {
+  // Block for text to sleep.
+  init() {
+    this.setColour(290);
+    this.appendValueInput('ELAPSE')
+      .setCheck('Number')
+      .appendField("Andar em frente por");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setInputsInline(true);
+  },
+};
+
+Blockly.Python.teste_blockly = function (block) {
+  // Generate Python for sleeping.
+  const elapse = Blockly.Python.valueToCode(block, 'ELAPSE',
+    Blockly.Python.ORDER_NONE) || '\'\'';
+  return `qualquer coisa\n`;
+};
+
+var toolbox = {
+  "kind": "flyoutToolbox",
+  "contents": [
+    {
+      "kind": "block",
+      "type": "controls_if"
+    },
+    {
+      "kind": "block",
+      "type": "controls_repeat_ext"
+    },
+    {
+      "kind": "block",
+      "type": "logic_compare"
+    },
+    {
+      "kind": "block",
+      "type": "math_number"
+    },
+    {
+      "kind": "block",
+      "type": "math_arithmetic"
+    },
+    {
+      "kind": "block",
+      "type": "text"
+    },
+    {
+      "kind": "block",
+      "type": "text_print"
+    },
+    {
+      "kind": "block",
+      "type": "text_trim"
+    }
+  ]
+}
+let workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox});
+let bloco = document.getElementById("blocklyDiv")
+console.log(bloco)
+
+
+const pythonCode = Blockly.Python.workspaceToCode(workspace);
+
