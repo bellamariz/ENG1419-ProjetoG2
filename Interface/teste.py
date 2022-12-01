5e9d02ca1439a0d68fdab8a41c10af590eec546c
linkad0 <phpg25@gmail.com>
2022-12-01 17:55:49 -0300

    Added import car and creation of car instantiation to blockly created string

--- Interface/teste.py
+++ Interface/teste.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def homepage():
     if request.method == 'POST':
-        language = request.get_data(as_text=True)
+        language = """
+        from models.car import Car
+        car = Car()
+
+        """
+        language += request.get_data(as_text=True)
         print(language)
     return render_template("index.html")
 
