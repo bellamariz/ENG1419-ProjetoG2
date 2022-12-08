16f244b85c3d81e80180b359e398870aaf34aecf
bellamariz <bellamariz@aluno.puc-rio.br>
2022-12-08 16:49:37 -0300

    Final calibration for car motors

--- motor-teste/control.py
+++ motor-teste/control.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 from gpiozero import Motor, Button, LED
 from time import sleep
 import encoder
+from datetime import datetime, timedelta
 
 
 
@@ -9,14 +10,23 @@ import encoder
 # VARIAVEIS GLOBAIS E CONSTANTES
 ################
 
-global razaoEncoders, gapCounter1, gapCounter2, anguloDoCarro, inputDirecao, inputAngulo, inputVelocidade
-razaoEncoders = 0     # gapCounter1 / gapCounter2
-gapCounter1 = 0       # total de gaps lidos (entre 0 e 20)
-gapCounter2 = 0       # total de gaps lidos (entre 0 e 20)
+global razaoEncoders, gapCounterEsq, gapCounterDir, anguloDoCarro, distDoCarro, inputDirecao, inputAngulo, inputVelocidade, inputDistancia
+global terminou, tempoAtualEsq, tempoInicialEsq, tempoAtualDir, tempoInicialDir
+razaoEncoders = 0     # gapCounterEsq / gapCounterDir
+gapCounterEsq = 0       # total de gaps lidos (entre 0 e 20)
+gapCounterDir = 0       # total de gaps lidos (entre 0 e 20)
 anguloDoCarro = 0.0
+distDoCarro = 0.0
 inputAngulo = 0.0          # entre -180 (esq) e 180 (dir) graus
 inputVelocidade = 0.0      # entre 0 e 1
 inputDirecao = ""          # frente ou tras
+inputDistancia = 0.0
+terminou = False
+tempoInicialEsq = None
+tempoAtualEsq = None
+tempoInicialDir = None
+tempoAtualDir = None
+
 
 # H Bridge GPIO pins
 # IN3,IN4 (left motor); IN1,IN2 (right motor);
@@ -41,15 +51,20 @@ motorEsq = Motor(HBRIDGE_IN3, HBRIDGE_IN4) # IN3 E IN4 (motor esquerda)
 motorDir = Motor(HBRIDGE_IN1, HBRIDGE_IN2) # IN1 E IN2 (motor direita) 
 
 def initialize():
-  global inputDirecao, inputAngulo, inputVelocidade
+  global inputDirecao, inputAngulo, inputVelocidade, inputDistancia, tempoInicialEsq, tempoInicialDir
 
   inputDirecao = input("Para qual direcao deseja andar, 'FRENTE', 'TRAS' ou 'NENHUMA'?\n")
+  
+  if inputDirecao == 'FRENTE' or inputDirecao == 'TRAS':
+    inputDistancia = float(input("Quanto de distancia deseja andar? (em m)\n"))*100
+  else:
+    inputDistancia = 0.0
   inputAngulo = float(input("Para qual angulo deseja girar (em graus)?\n"))
   inputVelocidade = float(input("Com quanto de velocidade, entre 0 e 100 porcento?\n"))/100
 
-  print("Direcao: %s\n Angulo %.2f graus\n Velocidade: %.2f\n"%(inputDirecao, inputAngulo, inputVelocidade*100))
-
-
+  print("Direcao: %s\n Angulo %.2f graus\n Velocidade: %.2f\n"%(inputDirecao, inputAngulo, inputVelocidade*100)) 
+  tempoInicialEsq = datetime.now()
+  tempoInicialDir = datetime.now()
 
 ################
 # CONTROLE DO MOTOR
@@ -96,25 +111,32 @@ def girar():
     andar()
 
 def giro_esquerda():
-  global inputAngulo, anguloDoCarro, gapCounter1
+  global inputAngulo, anguloDoCarro, gapCounterEsq, terminou
 
-  anguloDoCarro = encoder.calculaAnguloDoCarrinho(gapCounter1)
+  anguloDoCarro = encoder.calculaAnguloDoCarrinho(gapCounterEsq)
+  
+  print("Gaps: %d - Angulo: %.3f"%(gapCounterEsq, anguloDoCarro))
 
-  if anguloDoCarro < abs(inputAngulo):
+  if anguloDoCarro < abs(inputAngulo)-10:
     esquerda()
   else:
-    print("Terminou de girar! - Angulo Atual: %.4f"%(anguloDoCarro))
+    terminou = True
+    print("Terminou de girar! - Angulo Atual: %.3f"%(anguloDoCarro))
     parar()
 
+
 def giro_direita():
-  global inputAngulo, anguloDoCarro, gapCounter1
+  global inputAngulo, anguloDoCarro, gapCounterDir, terminou
 
-  anguloDoCarro = encoder.calculaAnguloDoCarrinho(gapCounter1)
+  anguloDoCarro = encoder.calculaAnguloDoCarrinho(gapCounterDir)
+  
+  print("Gaps: %d - Angulo: %.3f"%(gapCounterDir, anguloDoCarro))
 
-  if anguloDoCarro < abs(inputAngulo):
+  if anguloDoCarro < abs(inputAngulo)-10:
     direita()
   else:
-    print("Terminou de girar! - Angulo Atual: %.4f"%(anguloDoCarro))
+    terminou = True
+    print("Terminou de girar! - Angulo Atual: %.3f"%(anguloDoCarro))
     parar()
 
 
@@ -125,14 +147,44 @@ def giro_direita():
 
 def andar():
   global inputDirecao
-
+  
   if inputDirecao == "FRENTE":
-    frente()
+    andar_frente()
   elif inputDirecao == "TRAS":
-    tras()
+    andar_tras()
   else:
     parar()
 
+def andar_frente():
+  global inputDistancia, distDoCarro, gapCounterEsq, terminou, inputVelocidade
+
+  distDoCarro = encoder.calculaArcoDaRoda(gapCounterEsq)
+  fator = 0.5
+  
+  print("Gaps: %d - Dist: %.3f"%(gapCounterEsq, distDoCarro/fator))
+
+  if distDoCarro < inputDistancia*fator:
+    frente()
+  else:
+    terminou = True
+    print("Terminou de andar! - Dist Atual: %.3f"%(distDoCarro/fator))
+    parar()
+
+
+def andar_tras():
+  global inputDistancia, distDoCarro, gapCounterEsq, terminou, inputVelocidade
+
+  distDoCarro = encoder.calculaArcoDaRoda(gapCounterEsq)
+  fator = 0.5
+  
+  print("Gaps: %d - Dist: %.3f"%(gapCounterEsq, distDoCarro/fator))
+
+  if distDoCarro < inputDistancia*fator:
+    tras()
+  else:
+    terminou = True
+    print("Terminou de andar! - Dist Atual: %.3f"%(distDoCarro/fator))
+    parar()
 
 
 ################
@@ -140,22 +192,26 @@ def andar():
 ################
 
 def encoder1_handler(pin):
-  global gapCounter1, gapCounter2
-  gapCounter1+=1
-
-  if gapCounter1 > 100:
-    gapCounter1 = 0
-    gapCounter2 = 0
+  global gapCounterEsq, tempoAtualEsq, tempoInicialEsq
+  tempoAtualEsq = datetime.now()
+  
+  if tempoAtualEsq - tempoInicialEsq > timedelta(milliseconds=50):
+    gapCounterEsq+=1
+    tempoInicialEsq = tempoAtualEsq
   
 def encoder2_handler(pin):
-  global gapCounter1, gapCounter2
-  gapCounter2+=1
+  global gapCounterDir, tempoAtualDir, tempoInicialDir
+  tempoAtualDir = datetime.now()
+  
+  if tempoAtualDir - tempoInicialDir > timedelta(milliseconds=50):
+    gapCounterDir+=1
+    tempoInicialDir = tempoAtualDir
 
-  ratioEncoders()
+##  ratioEncoders()
 
 def ratioEncoders():
-  global razaoEncoders, gapCounter1, gapCounter2
-  razaoEncoders = gapCounter1/gapCounter2
+  global razaoEncoders, gapCounterEsq, gapCounterDir
+  razaoEncoders = gapCounterEsq/gapCounterDir
   print("Razao: %.5f"%(razaoEncoders))
 
 GPIO.add_event_detect(encoder.ENCODER1_SIGNAL_PIN, GPIO.RISING, encoder1_handler)
@@ -168,3 +224,12 @@ GPIO.add_event_detect(encoder.ENCODER2_SIGNAL_PIN, GPIO.RISING, encoder2_handler
 ################
 
 initialize()
+
+while True:
+  if not terminou:
+    #andar()
+    girar()
+  else:
+    terminou = False
+    print("Terminou!")
+    break
